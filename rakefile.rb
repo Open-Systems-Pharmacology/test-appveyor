df6071d5294e9f998401b75b1ab63c15ceb39831
Michael Sevestre <michael@design2code.ca>
2020-03-08 14:20:08 -0400

    Initial commit

--- rakefile.rb
+++ rakefile.rb
@@ -25,11 +25,8 @@ task :create_linux_build, [:product_version, :build_dir] do |t, args|
   command_line = %W[xzf #{tar_file} -C #{temp_dir}]
   Utils.run_cmd('tar', command_line)
 
-  puts "Temp Directory exists #{Dir.exists?(temp_dir)}"
-  puts "OSPSUITE  Directory exists #{Dir.exists?( File.join(temp_dir, "ospsuite"))}"
 
-
-  command_line = %W[cvzf  #{File.join(build_dir, "test.tar.gz")}  #{File.join(temp_dir, "ospsuite")}]
+   command_line = %W[czf #{File.join(build_dir, "test.tar.gz")}   -C #{temp_dir} ospsuite]
   Utils.run_cmd('tar', command_line)
 
   # ospsuite_dir = File.join(temp_dir,  'ospsuite')
